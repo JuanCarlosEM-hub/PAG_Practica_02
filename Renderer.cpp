4ccc5d88608d568221ed2db020500eb4b0e71b23
Juan Carlos Enríquez Muñoz <jcem0005@red.ujaen.es>
2026-09-22 17:41:46 +0200

    [desarrollo] Funcion Renderer getInstancia implementada

--- Renderer.cpp
+++ Renderer.cpp
@@ -2,3 +2,10 @@
 
 //Inicialización del atributo
 PAG::Renderer* PAG::Renderer::instance = nullptr;
+
+PAG::Renderer& PAG::Renderer::getInstance() {
+    if (!PAG::Renderer::instance) {//Inicialización perezosa
+        instance = new PAG::Renderer();
+    }
+    return *instance;
+}
