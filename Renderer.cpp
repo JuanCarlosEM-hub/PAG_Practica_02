600746adf727ef832e598c08ca4697d3c26935cf
Juan Carlos Enríquez Muñoz <jcem0005@red.ujaen.es>
2026-09-22 17:39:01 +0200

    [desarrollo] Comentarios eliminados

--- Renderer.cpp
+++ Renderer.cpp
@@ -1,7 +1,3 @@
-//
-// Created by Juank on 21/09/2026.
-//
-
 #include "Renderer.h"
 
 //Inicialización del atributo
--- Renderer.h
+++ Renderer.h
@@ -1,7 +1,3 @@
-//
-// Created by Juank on 21/09/2026.
-//
-
 #ifndef PR02_RENDERER_H
 #define PR02_RENDERER_H
 
