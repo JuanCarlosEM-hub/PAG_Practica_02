44467b515c32d7af3a50a8026b27ff393df35311
Juan Carlos Enríquez Muñoz <jcem0005@red.ujaen.es>
2026-09-23 19:29:21 +0200

    [desarrollo] Bibliotecas añadidas a GUI.h

--- GUI.h
+++ GUI.h
@@ -2,6 +2,15 @@
 // Created by Juank on 23/09/2026.
 //
 
+#include <ImGui/imgui.h>
+#include <ImGui/imgui_impl_glfw.h>
+#include <ImGui/imgui_impl_opengl3.h>
+#include <imgui.h>
+#include <../backends/imgui_impl_glfw.h>
+#include <../backends/imgui_impl_opengl3.h>
+
+#include <GLFW/glfw3.h>
+
 #ifndef PR02_GUI_H
 #define PR02_GUI_H
 
